df6b140f682af9cfc0e6d87e2fd00bf85a45564f
ja4343 <james.alvey@ectp.com>
2018-08-08 10:58:32 +0100

    First Commit
    First commit of in progress ML library

---
+++ ja_model/ja_model/models/__init__.py
@@ -0,0 +1,7 @@
+"""
+Submodule containing model implementations.
+"""
+
+from ._gen_model import Model
+from ._LinReg import LinReg
+from ._SVR import SVR
---
+++ ja_model/ja_model/utils/__init__.py
@@ -0,0 +1,6 @@
+"""
+Submodule containing utility functions for generating data structures and plotting standard outputs of the various models.
+"""
+
+from .data import random_date_range, powerset, _test_metric, _rmse
+from .plotting import *
